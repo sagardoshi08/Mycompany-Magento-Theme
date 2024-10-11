72c7e8581b67955c83ded98ea1fa33247c4b7e79
Sagar Doshi <sagard@elementsgs.com>
2024-10-11 19:33:21 +0530

    Magento Theme

---
+++ Custom/Mycompany/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    "deps": [
+        "js/custom"
+    ],
+    paths: {
+        'owlcarousel': 'js/owl.carousel.min'
+    },
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    }
+};
